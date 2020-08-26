f9e14ecf78354cf8451fdd9eb4dbb4d9de5816ad
2center <support@2center.net>
2020-08-26 16:34:56 +0800

    Add Google SignIn methods authentification in Firebase

--- app/src/main/java/com/druzbanarodov/relativlayoutjava/MainActivity.java
+++ app/src/main/java/com/druzbanarodov/relativlayoutjava/MainActivity.java
@@ -1,5 +1,6 @@
 package com.druzbanarodov.relativlayoutjava;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.ContextWrapper;
@@ -32,6 +33,11 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
@@ -45,9 +51,10 @@ public class MainActivity extends AppCompatActivity
     private FirebaseAuth mAuth;
 
     private static final String TAG = "EmailPassword";
+    private static final int RC_SIGN_IN = 5;
 
     //variables
-    Button show, show2, getStarted, Continue;
+    Button show, show2, getStarted, Continue, google_signIn_btn;
     EditText edit_password, edit_name, edit_email, edit_password2;
     TextView toast, name_display, forget;
     private final String Default = "N/A";
@@ -79,6 +86,8 @@ public class MainActivity extends AppCompatActivity
         progressBar.setProgress(0);
         progressBar.setMax(100);
 
+
+
         if (name_file.equals(Default) || pass_file.equals(Default) || email_file.equals(Default) || gender_file.equals(Default))
         {
             // Пользователь не существует в телефоне
@@ -97,6 +106,7 @@ public class MainActivity extends AppCompatActivity
             spinner.setAdapter(adapter);
             spinner.setOnItemSelectedListener(new spinner());
             getStarted = (Button) findViewById(R.id.getStarted);
+            google_signIn_btn = (Button) findViewById(R.id.google_sign_in_button);
 
             getStarted.setOnClickListener(v ->
             {
@@ -136,29 +146,27 @@ public class MainActivity extends AppCompatActivity
             // Пользователь есть в телефоне
             asyncSignInOrCreateUser(email_file, pass_file, null);
         }
+
+        google_signIn_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
     }
 
-    /*
-                            //write data to Firebase
-                        mAuth.createUserWithEmailAndPassword(save_email, save_password)
-                                .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
-                                    @Override
-                                    public void onComplete(@NonNull Task<AuthResult> task) {
-                                        if (task.isSuccessful()) {
-                                            // Sign in success, update UI with the signed-in user's information
-                                            Log.d(TAG, "createUserWithEmail:success");
-                                            FirebaseUser user = mAuth.getCurrentUser();
-                                            updateUI(user);
-                                        } else {
-                                            // If sign in fails, display a message to the user.
-                                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                                            Toast.makeText(MainActivity.this, "Authentication failed.",
-                                                    Toast.LENGTH_SHORT).show();
-                                            updateUI(null);
-                                        }
-                                    }
-                                });
-     */
+    private void signIn() {
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        // Build a GoogleSignInClient with the options specified by gso.
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        System.out.println("Google Клиент: " + gso);
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
 
     private void asyncSignInOrCreateUser(final String email, final String password, final SharedPreferences.Editor editor)
     {
@@ -226,7 +234,34 @@ public class MainActivity extends AppCompatActivity
                 });
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            System.out.println("Google account " + account);
+            // Signed in successfully, show authenticated UI.
+            updateUIwithGoogle(account);
+            //if (editor != null) editor.commit();
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Toast.makeText(MainActivity.this, "Ошибка авторизации", Toast.LENGTH_SHORT).show();
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            //updateUIwithGoogle(null);
+        }
+    }
 
     //Used to show the help by triggering a toast
     public void showHelp(View view) {
@@ -390,6 +425,20 @@ public class MainActivity extends AppCompatActivity
     private void updateUI(FirebaseUser currentUser) {
 
 
+    }
+    private void updateUIwithGoogle(GoogleSignInAccount account) {
+        final SharedPreferences sharedPreferences = getSharedPreferences("Content_main", Context.MODE_PRIVATE);
+        final SharedPreferences.Editor editor = sharedPreferences.edit();
+        System.out.println("Email пользователя: " + account.getEmail());
+        editor.putString("name", account.getDisplayName());
+        editor.putString("email", account.getEmail());
+        //editor.putString("email", account.getEmail());
+        editor.commit();
+        Intent intent = new Intent(MainActivity.this, Navigation_Nome_Menu.class);
+        startActivity(intent);
+        MainActivity.this.finish();
+        progressBar.cancel();
+        Toast.makeText(MainActivity.this, "Успешная регистрация", Toast.LENGTH_SHORT).show();
     }
 
 
